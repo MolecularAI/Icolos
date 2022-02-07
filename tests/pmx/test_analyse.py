43ac161adb02bd3c1a5f03960ff8b551178e411f
Moore, Harry <harry.moore@astrazeneca.com>
2022-02-07 18:06:36 +0100

    update pmx_analyse to perform comparison with experimental ddGs

--- tests/pmx/test_analyse.py
+++ tests/pmx/test_analyse.py
@@ -63,3 +63,40 @@ class Test_PMXanalyse(unittest.TestCase):
         stat_inf = os.stat(os.path.join(self._test_dir, "resultsAll.csv"))
 
         self.assertGreater(stat_inf.st_size, 200)
+
+    def test_pmx_analyse_experimental_writeout(self):
+        step_conf = {
+            _SBE.STEPID: "prepare_simulations",
+            _SBE.STEP_TYPE: "pmx_analyse",
+            _SBE.EXEC: {
+                _SBE.EXEC_PREFIXEXECUTION: "module load GROMACS/2020.3-fosscuda-2019a",
+            },
+            _SBE.SETTINGS: {
+                _SBE.SETTINGS_ARGUMENTS: {
+                    _SBE.SETTINGS_ARGUMENTS_FLAGS: [],
+                    _SBE.SETTINGS_ARGUMENTS_PARAMETERS: {},
+                },
+                _SBE.SETTINGS_ADDITIONAL: {
+                    "exp_results": MAIN_CONFIG["PMX"]["EXP_DATA"]
+                },
+            },
+        }
+        step_pmx_analyse = StepPMXRunAnalysis(**step_conf)
+        step_pmx_analyse.work_dir = self._test_dir
+        step_pmx_analyse._workflow_object = WorkFlow()
+        step_pmx_analyse._workflow_object.workflow_data.perturbation_map = self.p_map
+        step_pmx_analyse.execute()
+
+        stat_inf = os.stat(
+            os.path.join(self._test_dir, "0ec09ef_4afa8f9/complex/analyse1/results.txt")
+        )
+
+        self.assertGreater(stat_inf.st_size, 19000)
+
+        stat_inf = os.stat(os.path.join(self._test_dir, "resultsAll.csv"))
+
+        self.assertGreater(stat_inf.st_size, 200)
+
+        stat_inf = os.stat(os.path.join(self._test_dir, "resultsSummary.csv"))
+
+        self.assertGreater(stat_inf.st_size, 130)
