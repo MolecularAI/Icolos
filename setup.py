46e20504c398cbb28dca64410cc345ed319bd2f7
Moore, Harry <harry.moore@astrazeneca.com>
2022-02-16 16:53:47 +0100

    udpate changelog for 1.7 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 setup(
     name="icolos",
     maintainer="Christian Margreitter, Harry Moore",
-    version="1.6.1",
+    version="1.7.0",
     url="https://github.com/MolecularAI/Icolos",
     packages=find_namespace_packages(where="src"),
     package_dir={"": "src"},
