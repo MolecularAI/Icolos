23dea2f0954fa74b65c5ee7752f47e65d637e3e2
Moore, Harry <harry.moore@astrazeneca.com>
2022-03-01 09:44:29 +0000

    add manual control over step ordering
    if `previous_step` has been set in settings.additional, this will override the standard step ordering when the preprocessor looks for the output files from the previous job.  Allows backward compatability without nvt equilibration

--- src/icolos/core/workflow_steps/pmx/prepare_simulations.py
+++ src/icolos/core/workflow_steps/pmx/prepare_simulations.py
@@ -92,6 +92,8 @@ class StepPMXPrepareSimulations(StepPMXBase, BaseModel):
         """
         Works out where to get starting structure from based on the current run and simulation type
         """
+        if self.get_additional_setting(_PSE.PREV_STEP) is not None:
+            self.get_additional_setting(_PSE.PREV_STEP)
         if self.run_type == _PSE.RBFE:
             if sim_type == "nvt":
                 return "em"
