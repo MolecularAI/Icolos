045a053eb2ba93dd3543f9abf7968e4f530f2dd3
Moore, Harry <harry.moore@astrazeneca.com>
2022-03-09 14:52:39 +0100

    light refactor of active learning code, remove soap kernel

--- src/icolos/core/workflow_steps/active_learning/active_learning_reinvent.py
+++ src/icolos/core/workflow_steps/active_learning/active_learning_reinvent.py
@@ -48,84 +48,7 @@ class StepActiveLearning(ActiveLearningBase, BaseModel):
     def __init__(self, **data):
         super().__init__(**data)
 
-    def check_additional(self, key, val=True) -> bool:
-
-        if (
-            key in self.settings.additional.keys()
-            and self.settings.additional[key] == val
-        ):
-            return True
-        return False
-
-    def _initialize_oracle(self, compound_list: List[pd.Series]) -> List[StepBase]:
-        # list of step configs
-        base_oracle_config = self.settings.additional["oracle_config"]
-        oracle_steps = []
-        for step in base_oracle_config:
-            oracle_steps.append(self._initialize_oracle_step_from_dict(step))
-
-        # manually attach the compound objects to the oracle's lead step
-        # subsequent steps should take their input from the the first step.
-        for idx, compound in enumerate(compound_list):
-            cmp = Compound(compound_number=idx)
-            cmp.add_enumeration(
-                Enumeration(
-                    compound_object=cmp,
-                    smile=compound[_SALE.SMILES],
-                    molecule=compound[_SALE.MOLECULE],
-                )
-            )
-            oracle_steps[0].data.compounds.append(cmp)
-
-        return oracle_steps
-
-    def query_oracle(self, compound_list: List[Mol]) -> List[Compound]:
-        """
-        Interface function with the oracle method, in the most likely case this is ligprep + docking
-
-        Takes the requested compounds and runs them through the oracle workflow, returns the final compounds with annotations
-
-        Notes:
-        This could be an arbitrarily complex workflow, but the only thing that's going to change is the compounds.
-        """
-        oracle_steps = self._initialize_oracle(compound_list)
-        # we have a fully initialized step with the compounds loaded.  Execute them
-        for idx, step in enumerate(oracle_steps):
-            # for subsequent steps we will need to read in from the previous one
-            if idx != 0:
-                step.generate_input()
-            step.execute()
-            step.process_write_out()
-
-        # retrieve compounds from the final step
-        final_compounds = oracle_steps[-1].data.compounds
-        return final_compounds
-
-    def _extract_final_scores(
-        self, compounds: List[Compound], criteria: str, highest_is_best: bool = False
-    ) -> np.ndarray:
-        """
-        Takes a list of compound objects from the oracle and extracts the best score based on the provided criteria
-        """
-        top_scores = []
-        for comp in compounds:
-            scores = []
-            for enum in comp.get_enumerations():
-                for conf in enum.get_conformers():
-                    scores.append(float(conf._conformer.GetProp(criteria)))
-
-            # if docking generated no conformers
-            if not scores:
-                scores.append(0.0)
-
-            best_score = max(scores) if highest_is_best else min(scores)
-            top_scores.append(best_score)
-
-        return np.absolute(top_scores)
-
-    def _parse_library(
-        self, extract_property: bool = True
-    ) -> Tuple[DataFrame, np.ndarray]:
+    def _parse_library(self, criteria: str = None) -> Tuple[DataFrame, np.ndarray]:
         """
         Loads the library file from disk
         This should be a .sdf file containing the compounds to be screened
@@ -146,12 +69,8 @@ class StepActiveLearning(ActiveLearningBase, BaseModel):
         library = self.construct_fingerprints(library)
         print(library.head())
         scores = (
-            np.absolute(
-                pd.to_numeric(
-                    library[self.settings.additional[_SALE.CRITERIA]].fillna(0)
-                )
-            )
-            if extract_property
+            np.absolute(pd.to_numeric(library[criteria].fillna(0)))
+            if criteria is not None
             else []
         )
 
@@ -189,22 +108,6 @@ class StepActiveLearning(ActiveLearningBase, BaseModel):
             learner = ActiveLearner(
                 estimator=regressor,
                 query_strategy=greedy_acquisition,
-            )
-        elif running_mode == "soap_gpr":
-            device = "cuda" if torch.cuda.is_available() else "cpu"
-            print(device)
-            gpr = ExactGPRegressor(
-                SOAP_GP,
-                optimizer=torch.optim.Adam,
-                lr=0.1,
-                max_epochs=100,
-                device=device,
-                batch_size=-1,
-                module__tmp_dir=self._make_tmpdir(),
-                module__device=device,
-            )
-            learner = ActiveLearner(
-                gpr, query_strategy=self.greedy_acquisition, force_all_finite=False
             )
         else:
             raise KeyError(f"running mode: {running_mode} not supported")
@@ -271,94 +174,11 @@ class StepActiveLearning(ActiveLearningBase, BaseModel):
                 fraction_top1_hits.append(hits_queried)
         self._logger.log(f"Hits queried at each epoch:\n{fraction_top1_hits}", _LE.INFO)
 
-    def run_soap_learning_loop(
-        self, learner: ActiveLearner, lib, debug: bool = False, top_1_idx: list = None
-    ):
-        """
-        Learning loop specifically for soap descriptors
-        """
-        tmp_dir = self._make_tmpdir()
-        rounds = int(self.settings.additional[_SALE.N_ROUNDS])
-        n_instances = int(self.settings.additional[_SALE.BATCH_SIZE])
-        queried_compound_idx = []
-        fraction_top1_hits = []
-        key = _SALE.IDX
-        X = np.array(list(lib[key]))
-        for rnd in range(rounds):
-            query_idx, _ = learner.query(
-                X,
-                n_instances=n_instances,
-                previous_idx=queried_compound_idx,
-            )
-            print(query_idx)
-            queried_compound_idx += list(query_idx)
-            query_compounds = [lib.iloc[int(idx)] for idx in query_idx]
-
-            if not debug:
-                # get scores from oracle
-                self._logger.log(
-                    f"Querying oracle with {len(query_compounds)} compounds", _LE.INFO
-                )
-
-                compounds = self.query_oracle(query_compounds)
-                # also need to write the conformers to a tmpdir, use these shapes for soap descriptors
-                for compound, idx in zip(compounds, query_idx):
-                    # TODO: implement filtering step down to best per enumeration here
-                    enumeration = compound.get_enumerations()[0]
-                    try:
-                        conformer = enumeration.get_conformers()[0]
-                    except:
-                        # conformer wasn't found, docking failed, use a UFF minimization on the embedding
-                        conformer = AllChem.EmbedMolecule(enumeration.get_molecule())
-                        AllChem.UFFOptimizeMolecule(conformer)
-                    conformer.write(os.path.join(tmp_dir, f"{str(idx)}.sdf"))
-                os.listdir(tmp_dir)
-                scores = self._extract_final_scores(
-                    compounds, self.settings.additional[_SALE.CRITERIA]
-                )
-            else:
-                # retrieve precalculated scores instead of docking
-                self._logger.log("Retrieving scores from precomputed data...", _LE.INFO)
-                compounds = [lib.iloc[int(idx)][_SALE.MOLECULE] for idx in query_idx]
-                for comp, idx in zip(compounds, query_idx):
-                    writer = Chem.SDWriter(os.path.join(tmp_dir, f"{str(idx)}.sdf"))
-                    writer.write(comp)
-                scores = np.absolute(
-                    [
-                        float(
-                            lib.iloc[int(idx)][self.settings.additional[_SALE.CRITERIA]]
-                        )
-                        for idx in query_idx
-                    ],
-                    dtype=np.float32,
-                )
-                print(scores.shape)
-            self._logger.log("Fitting with new data...", _LE.INFO)
-            new_data = np.array([int(compound[key]) for compound in query_compounds])
-            print("new data shape", new_data.shape)
-            #
-
-            learner.teach(new_data, scores)
-            # calculate percentage of top-1% compounds queried
-            if top_1_idx is not None:
-                print(queried_compound_idx)
-                # what fraction of top1% compoudnds has the model requested to evaluate
-                hits_queried = (
-                    np.in1d(np.unique(np.array(queried_compound_idx)), top_1_idx).sum()
-                    / len(top_1_idx)
-                ) * 100
-                self._logger.log(
-                    f"Fraction of top 1% hits queries by round {rnd}: {hits_queried}%",
-                    _LE.INFO,
-                )
-                fraction_top1_hits.append(hits_queried)
-        self._logger.log(f"Hits queried at each epoch:\n{fraction_top1_hits}", _LE.INFO)
-
     def execute(self):
         tmp_dir = self._make_tmpdir()
         print(tmp_dir)
         lib, scores = self._parse_library(
-            extract_property=self.settings.additional["evaluate"]
+            criteria=self.get_additional_setting(_SALE.CRITERIA, default=None)
         )
         if scores is not None:
             top_1_percent = int(0.01 * len(scores))
@@ -366,20 +186,10 @@ class StepActiveLearning(ActiveLearningBase, BaseModel):
 
         learner = self._initialize_learner()
 
-        # if we are using a pre-calculated dataset for evaluation
-        if self.check_additional("evaluate"):
-            self._logger.log("Starting virtual library run...", _LE.DEBUG)
-            self.run_soap_learning_loop(
-                learner=learner, lib=lib, debug=True, top_1_idx=list(top_1_idx)
-            )
-        else:
-            self._logger.log("Starting on-the-fly run", _LE.DEBUG)
-            self.run_soap_learning_loop(
-                learner=learner,
-                lib=lib,
-                debug=False,
-                top_1_idx=list(top_1_idx),
-            )
+        self._logger.log("Evaluating virtual library...", _LE.DEBUG)
+        self._run_learning_loop(
+            learner=learner, lib=lib, debug=True, top_1_idx=list(top_1_idx)
+        )
 
         # pickle the final model
         # with open(os.path.join(tmp_dir, "model.pkl"), "wb") as f:
--- src/icolos/core/workflow_steps/active_learning/base.py
+++ src/icolos/core/workflow_steps/active_learning/base.py
@@ -1,7 +1,9 @@
 import os
 import tempfile
+from typing import List
 from pydantic import BaseModel
 import pandas as pd
+from icolos.core.containers.compound import Compound, Enumeration
 from icolos.core.workflow_steps.step import StepBase
 from sklearn.ensemble import RandomForestRegressor
 import numpy as np
@@ -10,6 +12,7 @@ from icolos.utils.enums.step_enums import StepActiveLearningEnum, StepBaseEnum
 from icolos.utils.general.convenience_functions import nested_get
 from icolos.utils.enums.step_initialization_enum import StepInitializationEnum
 from rdkit.Chem.AllChem import GetMorganFingerprintAsBitVect
+from rdkit.Chem import Mol
 
 _IE = StepInitializationEnum()
 _SALE = StepActiveLearningEnum()
@@ -83,3 +86,78 @@ class ActiveLearningBase(StepBase, BaseModel):
         # )
 
         return library
+
+    def _initialize_oracle(self, compound_list: List[pd.Series]) -> List[StepBase]:
+        # list of step configs
+        base_oracle_config = self.settings.additional["oracle_config"]
+        oracle_steps = []
+        for step in base_oracle_config:
+            oracle_steps.append(self._initialize_oracle_step_from_dict(step))
+
+        # manually attach the compound objects to the oracle's lead step
+        # subsequent steps should take their input from the the first step.
+        for idx, compound in enumerate(compound_list):
+            cmp = Compound(compound_number=idx)
+            cmp.add_enumeration(
+                Enumeration(
+                    compound_object=cmp,
+                    smile=compound[_SALE.SMILES],
+                    molecule=compound[_SALE.MOLECULE],
+                )
+            )
+            oracle_steps[0].data.compounds.append(cmp)
+
+        return oracle_steps
+
+    def query_oracle(self, compound_list: List[Mol]) -> List[Compound]:
+        """
+        Interface function with the oracle method, in the most likely case this is ligprep + docking
+
+        Takes the requested compounds and runs them through the oracle workflow, returns the final compounds with annotations
+
+        Notes:
+        This could be an arbitrarily complex workflow, but the only thing that's going to change is the compounds.
+        """
+        oracle_steps = self._initialize_oracle(compound_list)
+        # we have a fully initialized step with the compounds loaded.  Execute them
+        for idx, step in enumerate(oracle_steps):
+            # for subsequent steps we will need to read in from the previous one
+            if idx != 0:
+                step.generate_input()
+            step.execute()
+            step.process_write_out()
+
+        # retrieve compounds from the final step
+        final_compounds = oracle_steps[-1].data.compounds
+        return final_compounds
+
+    def _extract_final_scores(
+        self, compounds: List[Compound], criteria: str, highest_is_best: bool = False
+    ) -> np.ndarray:
+        """
+        Takes a list of compound objects from the oracle and extracts the best score based on the provided criteria
+        """
+        top_scores = []
+        for comp in compounds:
+            scores = []
+            for enum in comp.get_enumerations():
+                for conf in enum.get_conformers():
+                    scores.append(float(conf._conformer.GetProp(criteria)))
+
+            # if docking generated no conformers
+            if not scores:
+                scores.append(0.0)
+
+            best_score = max(scores) if highest_is_best else min(scores)
+            top_scores.append(best_score)
+
+        return np.absolute(top_scores)
+    
+    def check_additional(self, key, val=True) -> bool:
+
+        if (
+            key in self.settings.additional.keys()
+            and self.settings.additional[key] == val
+        ):
+            return True
+        return False
