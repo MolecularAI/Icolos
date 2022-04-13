45c1c41666fb453a498d31b65f7fc085cb4aa8d3
Moore, Harry <harry.moore@astrazeneca.com>
2022-04-13 12:02:18 +0200

    revert batching strategy in pmx run_simulations

--- src/icolos/core/workflow_steps/pmx/run_simulations.py
+++ src/icolos/core/workflow_steps/pmx/run_simulations.py
@@ -47,8 +47,10 @@ class StepPMXRunSimulations(StepPMXBase, BaseModel):
                 f"Prepared {len(job_pool)} jobs for {self.sim_type} simulations, branch {branch}",
                 _LE.DEBUG,
             )
-            # run this through in many batches of 1 to allow efficient scaling on cluster
-            self.execution.parallelization.max_length_sublists = 1
+            # run everything through in one batch, with multiple edges per call
+            self.execution.parallelization.max_length_sublists = int(
+                np.floor(len(job_pool) / self._get_number_cores())
+            )
             self._subtask_container = SubtaskContainer(
                 max_tries=self.execution.failure_policy.n_tries
             )
