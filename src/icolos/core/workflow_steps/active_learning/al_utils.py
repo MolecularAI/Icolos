ca23f33eda7a3a0de9a70429d4be3776e9c31e11
Moore, Harry <harry.moore@astrazeneca.com>
2022-04-21 11:10:21 +0000

    add EI to al_utils

--- src/icolos/core/workflow_steps/active_learning/al_utils.py
+++ src/icolos/core/workflow_steps/active_learning/al_utils.py
@@ -2,6 +2,7 @@ from typing import List
 import numpy as np
 from icolos.loggers.steplogger import StepLogger
 from icolos.core.workflow_steps.step import _LE
+from scipy.stats import norm
 
 _logger = StepLogger()
 
@@ -61,3 +62,41 @@ def greedy_acquisition(
         for idx, val in zip(indices_to_replace, replacements):
             predictions[idx] = val
     return sorted_preds
+
+
+def expected_improvement(
+    estimator,
+    X: np.ndarray,
+    previous_idx: List[int],
+    n_instances: int,
+    warmup: bool = False,
+    n_repeats: int = 5,
+    highest_is_best: bool = False,
+) -> np.ndarray:
+    """Select new points to acquire based on expected improvement
+
+    :param  estimator: SKLearn-type estimator to be queried
+    :param np.ndarray X: array of fingerprints for each compound to be predicted by the model
+    :param List[int] previous_idx: List of the previously queried indices to avoid repitition
+    :param int n_instances: batch size to query
+    :param bool warmup: Control warmup period in which random samples are generated, defaults to False
+    :param float epsilon: enable greedy epsilon acquisition, defaults to 0.0
+    :return np.ndarray: array of top predictions from the estimator
+    """
+    # if no stdev implemented (only rf + GP can do this as std), generate the standard deviation from repeated sampling
+    if warmup:
+        _logger.log("Warmup epoch, using random sampling...", _LE.DEBUG)
+        return np.array([np.random.randint(0, X.shape[0]) for _ in range(n_instances)])
+
+    subEstimates = np.array([estimator.predict(np.array(X)) for _ in range(n_repeats)])
+    stdev = np.std(subEstimates, axis=0).reshape((-1))
+    y_hat = np.array(estimator.predict(X))
+    mu = np.mean(y_hat)
+    y_best = np.max(y_hat) if highest_is_best else np.min(y_hat)
+    gamma = (mu - y_best) / stdev
+    ei = stdev * gamma * norm.cdf(gamma) + stdev * norm.pdf(gamma)
+
+    for idx in previous_idx:
+        ei[idx] = 0
+    # smaller before n_instances, largest after
+    return np.argpartition(ei, -n_instances, axis=0)[-n_instances:]
