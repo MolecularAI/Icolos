c661785ddda6d5fff3872b04fc8c79083fd73491
Moore, Harry <harry.moore@astrazeneca.com>
2022-05-04 14:33:06 +0000

    fix bug in data_manipulation when filtering at compound level

--- src/icolos/core/workflow_steps/io/data_manipulation.py
+++ src/icolos/core/workflow_steps/io/data_manipulation.py
@@ -1,7 +1,7 @@
 from typing import List, Union
 from pydantic import BaseModel
 
-from icolos.core.containers.compound import unroll_conformers
+from icolos.core.containers.compound import Compound, unroll_conformers
 from icolos.core.step_utils.structcat_util import StructcatUtil
 from icolos.core.step_utils.structconvert import StructConvert
 from icolos.utils.enums.program_parameters import (
@@ -135,6 +135,8 @@ class StepDataManipulation(StepIOBase, BaseModel):
                     for conf in top_confs:
                         top_conformer_list.append(conf)
         if self.settings.additional[_SFE.FILTER_LEVEL] == _SFE.COMPOUNDS:
+            self._logger.log("Filtering to top conformers per compound", _LE.DEBUG)
+
             # sort the top conformers from each enumeration and attach the top n conformers to their respective enumeration, get rid of the rest
             # sorted_top_confs = sorted(top_conformer_list,
             #                           key=lambda x: x.get_molecule().GetProp(self.settings.additional[_SFE.CRITERIA]),
@@ -146,12 +148,36 @@ class StepDataManipulation(StepIOBase, BaseModel):
                 reverse=reverse,
                 aggregation=aggregation,
             )
+
             for compound in self.data.compounds:
                 for enum in compound.get_enumerations():
                     enum.clear_conformers()
             for conf in sorted_top_confs:
                 enum = conf.get_enumeration_object()
-                enum.add_conformer(conf)
+                # if that enum's compound object already has top_n attached, skip
+                comp: Compound = enum.get_compound_object()
+                total_confs = self.get_conformer_count(comp)
+                if total_confs < top_n:
+                    enum.add_conformer(conf)
+                else:
+                    self._logger.log(
+                        "Already reached max conformers, skipping", _LE.DEBUG
+                    )
+
+            # remove empty conformers
+
+    def get_conformer_count(self, comp: Compound) -> int:
+        """count attached conformers over all enumerations
+
+        :param Compound comp: compound to inspect
+        :return int: number of conformers over all enumerations
+        """
+        total_confs = 0
+        for enum in comp.get_enumerations():
+            for conf in enum.get_conformers():
+                total_confs += 1
+
+        return total_confs
 
     def _sort_conformers(
         self,
