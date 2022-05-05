a83726528d7514be0333924a412a072233faeed8
Moore, Harry <harry.moore@astrazeneca.com>
2022-05-05 18:01:27 +0200

    fix residue scanning output parsing

--- src/icolos/core/workflow_steps/schrodinger/residue_scanning.py
+++ src/icolos/core/workflow_steps/schrodinger/residue_scanning.py
@@ -45,17 +45,32 @@ class StepResidueScanning(StepSchrodingerBase, BaseModel):
         # parse csv file, attach to compounds
         compounds = self.get_compounds()
         props_df = pd.read_csv("props_out.csv")
-        for idx, comp in enumerate(compounds):
+        for comp in compounds:
             mol = comp.get_enumerations()[0].get_molecule()
-            # row 0 is the wild type
-            row = props_df.iloc[idx + 1]
-            mol.SetProp(
-                "r_bioluminate_delta_Stability",
-                str(row["r_bioluminate_delta_Stability"]),
-            )
-            mol.SetProp(
-                "r_bioluminate_delta_Affinity", str(row["r_bioluminate_delta_Affinity"])
-            )
+
+            # find the row by the name
+            mol_name = mol.GetProp("_Name")
+            row = props_df.loc[
+                props_df["s_bioluminate_Mutations"].str.contains(mol_name)
+            ]
+            try:
+                mol.SetProp(
+                    "r_bioluminate_delta_Stability",
+                    str(row["r_bioluminate_delta_Stability"]),
+                )
+                mol.SetProp(
+                    "r_bioluminate_delta_Affinity",
+                    str(row["r_bioluminate_delta_Affinity"]),
+                )
+            except KeyError:
+                mol.SetProp(
+                    "r_bioluminate_delta_Stability",
+                    str(0.00),
+                )
+                mol.SetProp(
+                    "r_bioluminate_delta_Affinity",
+                    str(0.00),
+                )
 
     def execute(self):
         tmp_dir = self._make_tmpdir()
